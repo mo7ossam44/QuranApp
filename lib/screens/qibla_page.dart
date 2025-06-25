1ff3fc03f63a5b29f06f197790dc644c6aa6b50f
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-25 19:36:05 +0300

    add qibla Page

---
+++ lib/screens/qibla_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QiblaPage extends StatelessWidget {
+  const QiblaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
