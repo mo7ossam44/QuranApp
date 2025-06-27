83b9a91ac872c381a8d14bdc5bd078d1c6ec2956
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-27 03:45:10 +0300

    add MH widget

---
+++ lib/Widgets/mohamed_hossam_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MohamedHossamWidget extends StatelessWidget {
+  const MohamedHossamWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Mohamed Hossam',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Antic',
+      ),
+    );
+  }
+}
