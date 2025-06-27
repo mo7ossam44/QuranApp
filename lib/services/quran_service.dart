8be0236b0a9333fdfb2c78af73f4c3506c0a13e3
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-27 03:44:15 +0300

    add getAyah Method and edit getSurah

--- lib/services/quran_service.dart
+++ lib/services/quran_service.dart
@@ -1,25 +1,45 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:quran_app/models/ayat_model.dart';
 import 'package:quran_app/models/surah_model.dart';
 
 class QuranService {
   final dio = Dio();
 
   Future<List<SurahModel>> getSurah() async {
-    Response response = await dio.get('http://api.alquran.cloud/v1/surah');
+    Response response = await dio.get('https://api.alquran.cloud/v1/surah');
     Map<String, dynamic> jsonData = response.data;
-    List<dynamic> surahData = jsonData['data'];
+    List<dynamic> data = jsonData['data'];
     List<SurahModel> surahModelList = [];
-    for (var surah in surahData) {
+    for (var surah in data) {
       SurahModel surahModel = SurahModel(
-        number: surah['number'],
         name: surah['name'],
+        number: surah['number'],
         englishName: surah['englishName'],
-        englishNameTranslation: surah['englishNameTranslation'],
         numberOfAyahs: surah['numberOfAyahs'],
         revelationType: surah['revelationType'],
       );
       surahModelList.add(surahModel);
     }
     return surahModelList;
+  }
+
+  Future<List<AyatModel>> getAyah({required int surahNumber}) async {
+    Response response = await dio.get(
+      'https://api.alquran.cloud/v1/surah/$surahNumber/ar',
+    );
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> ayaData = jsonData['data']['ayahs'];
+    List<AyatModel> ayatModelList = [];
+    for (var ayah in ayaData) {
+      AyatModel aya = AyatModel(
+        page: ayah['page'],
+        text: ayah['text'],
+        numberInSurah: ayah['numberInSurah'],
+      );
+      ayatModelList.add(aya);
+    }
+    return ayatModelList;
   }
 }
