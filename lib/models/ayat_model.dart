df3629f71e32b26ccb2378fb74ef1ef6307eedb5
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-27 03:41:35 +0300

    add ayat model

---
+++ lib/models/ayat_model.dart
@@ -0,0 +1,12 @@
+class AyatModel {
+  final int page;
+  final int numberInSurah;
+  final String text;
+
+
+  AyatModel({
+    required this.page,
+    required this.numberInSurah,
+    required this.text,
+  });
+}
