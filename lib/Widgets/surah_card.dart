ee538869b7d7fbf2b0f007a575acf2d11a4cca8e
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-27 03:45:35 +0300

    add nvaigation widget in surah card

--- lib/Widgets/surah_card.dart
+++ lib/Widgets/surah_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/models/surah_model.dart';
+import 'package:quran_app/screens/ayat_page.dart';
 
 class SurahCard extends StatelessWidget {
   const SurahCard({super.key, required this.surahModel});
@@ -8,42 +9,59 @@ class SurahCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      color: Colors.white,
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 15,
-          backgroundColor: Colors.green,
-          child: Text(
-            '${surahModel.number}',
-            style: TextStyle(color: Colors.white),
-          ),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) =>
+              AyatPage(surahModel: surahModel, surahNumber: surahModel.number),
         ),
-        title: Row(
-          children: [
-            Text(
-              '${surahModel.englishName} ',
+      ),
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        color: Colors.white,
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 15,
+            backgroundColor: Colors.green,
+            child: Text(
+              '${surahModel.number}',
               style: TextStyle(
+                color: Colors.white,
                 fontFamily: 'Antic',
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text('(${surahModel.revelationType})'),
-          ],
-        ),
-        subtitle: Row(
-          children: [
-            Text('• ', style: TextStyle(color: Colors.green, fontSize: 20)),
-            Text(
-              surahModel.englishNameTranslation,
-              style: TextStyle(fontSize: 13),
+          ),
+          title: Row(
+            children: [
+              Text(
+                '${surahModel.englishName} ',
+                style: TextStyle(
+                  fontFamily: 'Antic',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '(${surahModel.revelationType})',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Antic',
+                ),
+              ),
+            ],
+          ),
+          subtitle: Text(
+            '${surahModel.numberOfAyahs} Ayah',
+            style: TextStyle(
+              color: Colors.green,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Antic',
             ),
-          ],
-        ),
-        trailing: Text(
-          surahModel.name.replaceAll('سُورَةُ', ''),
-          style: TextStyle(fontSize: 21, fontFamily: 'KFGQPC'),
+          ),
+          trailing: Text(
+            surahModel.name.replaceAll('سُورَةُ', ''),
+            style: TextStyle(fontSize: 21, fontFamily: 'KFGQPC'),
+          ),
         ),
       ),
     );
