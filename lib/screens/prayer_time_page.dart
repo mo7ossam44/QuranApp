71ec54cef14a9deb061ae742cb442914ad11049c
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-30 12:00:52 +0300

    add prayer time page

--- lib/screens/prayer_time_page.dart
+++ lib/screens/prayer_time_page.dart
@@ -5,6 +5,22 @@ class PrayerTimePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.green, size: 27),
+        title: Text(
+          'ميعاد الصلاة',
+          style: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'expo',
+          ),
+        ),
+      ),
+    );
   }
 }
