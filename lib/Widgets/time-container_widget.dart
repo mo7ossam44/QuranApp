52dc998680a0be2321b803560f064307fe9868ed
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-30 08:50:33 +0300

    add time in container and the greadiants of color

--- lib/Widgets/time-container_widget.dart
+++ lib/Widgets/time-container_widget.dart
@@ -1,18 +1,49 @@
+import 'dart:async';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
-class TimeContainerWidget extends StatelessWidget {
+class TimeContainerWidget extends StatefulWidget {
   const TimeContainerWidget({super.key});
+
+  @override
+  State<TimeContainerWidget> createState() => _TimeContainerWidgetState();
+}
+
+class _TimeContainerWidgetState extends State<TimeContainerWidget> {
+  String _timeString = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    Timer.periodic(Duration(seconds: 60), (Timer t) => _updateTime());
+  }
+
+  void _updateTime() {
+    final now = DateTime.now();
+    final formattedTime = DateFormat('h:mm a').format(now);
+    setState(() {
+      _timeString = formattedTime;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 160,
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 188, 232, 184),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 183, 255, 214), // أخضر فاتح جدًا
+            Color(0xFFDFF1E8), // أخضر أغمق حاجة بسيطة
+          ],
+        ),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(12),
+        padding: const EdgeInsets.all(18),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -25,11 +56,11 @@ class TimeContainerWidget extends StatelessWidget {
                   style: TextStyle(fontSize: 17, fontFamily: 'Antic'),
                 ),
                 Text(
-                  '10.45 Am',
+                  _timeString,
                   style: TextStyle(
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.normal,
                     fontSize: 40,
-                    fontFamily: 'Antic',
+                    fontFamily: 'expo',
                   ),
                 ),
                 Text(
@@ -42,7 +73,7 @@ class TimeContainerWidget extends StatelessWidget {
                 ),
               ],
             ),
-            Image.asset('assets/images/calendar.png', width: 100),
+            Image.asset('assets/images/clock2.png', width: 100),
           ],
         ),
       ),
