8fac13760332ff2fe4f6e1ac87372bc033fbbe53
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-30 12:00:20 +0300

    add azkar card

--- lib/screens/azkar_page.dart
+++ lib/screens/azkar_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/models/azkar_model.dart';
+import 'package:quran_app/models/zeker_category_model.dart';
+import 'package:quran_app/screens/home_page.dart';
+import 'package:quran_app/screens/single_zeker_page.dart';
 import 'package:quran_app/services/azkar_service.dart';
 
 class AzkarPage extends StatefulWidget {
-  const AzkarPage({super.key, required this.categoryName});
-  final String categoryName;
+  const AzkarPage({super.key, required this.zekerCategoryModel});
+  final ZekerCategoryModel zekerCategoryModel;
 
   @override
   State<AzkarPage> createState() => _AzkarPageState();
@@ -20,7 +23,7 @@ class _AzkarPageState extends State<AzkarPage> {
   }
 
   Future<List<AzkarModel>> getZeker() async => azkarList = await AzkarService()
-      .getAzkar(categoryNameString: widget.categoryName);
+      .getAzkar(categoryNameString: widget.zekerCategoryModel.category);
 
   @override
   Widget build(BuildContext context) {
@@ -32,21 +35,48 @@ class _AzkarPageState extends State<AzkarPage> {
         centerTitle: true,
         iconTheme: IconThemeData(color: Colors.green, size: 27),
         title: Text(
-          widget.categoryName,
-          style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold , fontFamily: 'expo'),
+          widget.zekerCategoryModel.category,
+          style: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'expo',
+          ),
         ),
       ),
       body: ListView.builder(
-        itemCount: azkarList.length,
+        itemCount: widget.zekerCategoryModel.array.length,
         itemBuilder: (context, index) {
-          return Container(
-            child: Column(
-              children: [
-                Text(
-                  azkarList[index].text,
-                  style: TextStyle(color: Colors.green),
+          final azkar = widget.zekerCategoryModel.array[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SingleZekerPage(
+                    azkarModel: azkar,
+                    zekerCategoryModel: widget.zekerCategoryModel,
+                  ),
                 ),
-              ],
+              );
+            },
+            child: Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    azkar.text,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'expo',
+                      letterSpacing: 0.5,
+                      color: Colors.green,
+                    ),
+                    textDirection: TextDirection.rtl,
+                  ),
+                ),
+              ),
             ),
           );
         },
