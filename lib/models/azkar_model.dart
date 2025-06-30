e5604304de374181f0ace8fbaa41a695ce590a6b
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-30 08:47:21 +0300

    edit azkar model

---
+++ lib/models/azkar_model.dart
@@ -0,0 +1,52 @@
+// class AzkarModel {
+//   final int id;
+//   final String text;
+//   final int count;
+//   final String audio;
+//   final String filename;
+
+//   AzkarModel({
+//     required this.id,
+//     required this.text,
+//     required this.count,
+//     required this.audio,
+//     required this.filename,
+//   });
+
+//    factory AzkarModel.fromJson(Map<String, dynamic> json) {
+//     return AzkarModel(
+//       id: json['id'] ?? '',
+//       text: json['text'] ?? '',
+//       count: json['count'] ?? 0,
+//       audio: json['audio'] ?? '',
+//       filename: json['filename'] ?? '',
+//     );
+//   }
+
+// }
+class AzkarModel {
+  final int id;
+  final int count;
+  final String text;
+  final String audio;
+  final String filename;
+
+  AzkarModel({
+    required this.id,
+    required this.text,
+    required this.count,
+    required this.audio,
+    required this.filename,
+  });
+
+  factory AzkarModel.fromJson(Map<String, dynamic> json) {
+    return AzkarModel(
+      id: json['id'],
+      text: json['text'],
+      count: json['count'],
+      audio: json['audio'],
+      filename: json['filename'],
+    );
+  }
+}
+
