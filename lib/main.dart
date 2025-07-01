d2c0a6647e568bcf7673d29b2e3c513cb085f536
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-01 21:05:51 +0300

    remove the comment in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/screens/home_page.dart';
+import 'package:quran_app/screens/first_page.dart';
+import 'package:quran_app/services/prayer_time_service.dart';
 
 void main() {
   runApp(const MyApp());
+  PrayerTimeService().getPrayerTimes();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()),
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: FirstPage()),
     );
   }
 }
