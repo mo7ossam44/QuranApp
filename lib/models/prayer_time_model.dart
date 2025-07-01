9d1f5dc8e567015c9444b5538d9c9e72b2609ca7
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-01 21:06:04 +0300

    add prayer time model

---
+++ lib/models/prayer_time_model.dart
@@ -0,0 +1,15 @@
+class PrayerTimeModel {
+  final String fajr;
+  final String dhuhr;
+  final String asr;
+  final String maghrib;
+  final String isha;
+
+  PrayerTimeModel({
+    required this.fajr,
+    required this.dhuhr,
+    required this.asr,
+    required this.maghrib,
+    required this.isha,
+  });
+}
